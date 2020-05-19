0de25b2b019074622fe5d990909acfad5237f48e
Kaio Dolfini <kaio.dolfini@gmail.com>
2020-05-18 21:09:08 -0300

    Adicionando Dados Dinâmicos ao Desafio 3

---
+++ data.js
@@ -0,0 +1,28 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "Starter",
+        image: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        modules: "5 Módulos",
+        price: "Free"
+    },
+    {
+        id: "launchbase",
+        title: "Launch Base Bootcamp",
+        image: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        modules: "14 Módulos",
+        price: "Paid"
+    },
+    {
+        id: "gostack",
+        title: "Go Stack Bootcamp",
+        image: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        modules: "6 weeks",
+        price: "Paid"
+    }
+]
+
+
+
+
+
--- server.js
+++ server.js
@@ -2,21 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const courses = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("view", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const about = {
+        title: "ROCKETSEAT",
+        subtitle: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        description1: "Mais do que uma plataforma de educação em tecnologia, somos uma comunidade incrível de programadores em busca do próximo nível 🚀",
+        description2: "Em um mundo onde a informação fica obsoleta cada vez mais rápido, velocidade de aprendizado é a chave para o sucesso.",
+        description3: "Por isso a Rocketseat oferece através de uma plataforma inteligente e metodologia prática, além de comunidade, eventos, conteúdo e conexão com o mercado de trabalho, todas as ferramentas que você precisa para masterizar no menor tempo possível as tecnologias mais modernas de desenvolvimento web e mobile, e dessa forma avançar para o próximo nível como programador.",
+        tecnologies: [
+            { name: "Javascript"},
+            { name: "Node JS"},
+            { name: "React JS"},
+            { name: "React Native"}
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/courses", function(req, res){
-    return res.render("courses")
+    return res.render("courses", { items: courses })
 })
 
 server.use(function(req, res) {
